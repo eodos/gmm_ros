d09f6de0631d7292dc17006161f26a9cba3b203b
David Paul Pena <dpaul@nyu.edu>
2017-05-04 10:23:49 -0400

    Add AsyncSerial library and set up CMakeLists file to compile it as a static library

---
+++ include/gmm/aplink.hpp
@@ -0,0 +1,4 @@
+#ifndef APLINK_H
+#define APLINK_H 1
+
+#endif
--- src/aplink.cpp
+++ src/aplink.cpp
@@ -1,6 +1,13 @@
 #include "ros/ros.h"
 
-int int main(int argc, char *argv[])
+#include "aplink.hpp"
+
+#include "BufferedAsyncSerial.h"
+#include <boost/chrono/chrono.hpp>
+#include <boost/thread/mutex.hpp>
+#include <boost/thread/thread.hpp>
+
+int main(int argc, char **argv)
 {
 	ros::init(argc, argv, "gmm_aplink");
 	ros::NodeHandle n;
